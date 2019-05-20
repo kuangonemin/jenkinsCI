8e07f099afaa1d7f5ea3cdf309abcc3d20574d5e
Xiang Kuang <kuangxiang@hicailiao.com>
2019-05-20 17:15:59 +0800

    new init project

---
+++ src/main/java/com/kuang/jenkins/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.kuang.jenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: kuangxiang
+ * @Description:
+ * @Date: Create in 17:09 2019/5/20
+ * @Modified By:
+ */
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index.htm")
+	public String reqIndex() {
+		return "index";
+	}
+}
---
+++ src/main/java/com/kuang/jenkins/utils/VelocityToolsView.java
@@ -0,0 +1,46 @@
+package com.kuang.jenkins.utils;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.velocity.context.Context;
+import org.apache.velocity.tools.Scope;
+import org.apache.velocity.tools.ToolManager;
+import org.apache.velocity.tools.view.ViewToolContext;
+import org.springframework.web.servlet.view.velocity.VelocityToolboxView;
+
+/**
+ * Adds support for Velocity Tools tools.
+ *
+ * @author zlei
+ * @see VelocityToolboxView
+ */
+public class VelocityToolsView extends VelocityToolboxView {
+
+	// Create a ChainedContext instance
+	protected Context createVelocityContext(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+		ViewToolContext ctx;
+		ctx = new ViewToolContext(getVelocityEngine(), request, response, getServletContext());
+		ctx.putAll(model);
+
+		if (this.getToolboxConfigLocation() != null) {
+			ToolManager tm = new ToolManager();
+			tm.setVelocityEngine(getVelocityEngine());
+			tm.configure(getServletContext().getRealPath(getToolboxConfigLocation()));
+			if (tm.getToolboxFactory().hasTools(Scope.REQUEST)) {
+				ctx.addToolbox(tm.getToolboxFactory().createToolbox(Scope.REQUEST));
+			}
+			if (tm.getToolboxFactory().hasTools(Scope.APPLICATION)) {
+				ctx.addToolbox(tm.getToolboxFactory().createToolbox(Scope.APPLICATION));
+			}
+			if (tm.getToolboxFactory().hasTools(Scope.SESSION)) {
+				ctx.addToolbox(tm.getToolboxFactory().createToolbox(Scope.SESSION));
+			}
+		}
+		return ctx;
+	}
+}
